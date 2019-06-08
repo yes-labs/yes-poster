aa78aa1b9d2f00f560c0617b40946e0da1974c79
Dale Harvey <dale@arandomurl.com>
2019-06-08 23:31:55 +0100

    Rewrite

--- www/script.js
+++ www/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const subHead = document.getElementById("subhead");
+const $ = document.querySelector.bind(document);
+
 const download = document.getElementById("download");
 const fullLogo = document.getElementById("full-logo");
 
@@ -21,36 +22,70 @@ function debounce(func, wait, immediate) {
 	};
 };
 
-function selectElementContents(el) {
-  var range = document.createRange();
-  range.selectNodeContents(el);
-  var sel = window.getSelection();
-  sel.removeAllRanges();
-  sel.addRange(range);
-}
+let canvas;
 
-function setEndOfContenteditable(contentEditableElement) {
-  let range = document.createRange();
-  range.selectNodeContents(contentEditableElement);
-  range.collapse(false);
-  let selection = window.getSelection();
-  selection.removeAllRanges();
-  selection.addRange(range);
-}
+const update = debounce(async () => {
 
-const update = debounce(() => {
-  let pos = subHead.innerText.length;
-  let divisor = Math.floor(subHead.innerText.length / colours.length) + 1;
-  let html = [];
-  for (let i = 0; i < subHead.innerText.length; i++) {
-    let character = subHead.innerText[i];
-    let colour = colours[Math.floor(i / divisor)];
-    html.push(`<span style="color: #${colour}">${character}</span>`);
-  }
-  let content = html.join("");
-  subHead.innerHTML = content;
-  setEndOfContenteditable(subHead);
+	let name = document.getElementById("name").value;
+	let radio = document.querySelector("input[name=layout]:checked");
+	let layout = radio && radio.value || "a4";
+
+	if (!(!name && layout === "a4")) {
+		let params = "?" + new URLSearchParams({name, layout}).toString();
+		window.history.replaceState("", "", params);
+	}
+	fullLogo.dataset.layout = layout;
+
+	let sizes = {
+		'a4': { width: 3508, height: 2480 },
+		'square': { width: 3000, height: 3000 }
+	};
+	let canvasSize = sizes[layout];
+
+  canvas = document.createElement("canvas");
+	const ctx = canvas.getContext('2d');
+
+	let width = canvas.width = canvasSize.width;
+	let height = canvas.height = canvasSize.height;
+
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, width, height);
+
+	let subHeadHeight = height / 10;
+
+  let img = await loadImage("yes.svg");
+	let marginTop = (height - (img.height + subHeadHeight)) / 2.2;
+
+	ctx.drawImage(img, (width - img.width) / 2, marginTop,
+										 img.width, img.height);
+
+	ctx.font = subHeadHeight + "px gilroyblack";
+	let size = ctx.measureText(name);
+	let y = img.height + marginTop + 350;
+	let divisor = Math.floor(name.length / colours.length) + 1;
+	let start = (width - size.width) / 2;
+
+	for (let i = 0; i < name.length; i++) {
+		ctx.fillStyle = "#" + colours[Math.floor(i / divisor)];
+		ctx.fillText(name[i], start, y);
+		start += ctx.measureText(name[i]).width;
+	}
+
+	fullLogo.innerHTML = '';
+	fullLogo.appendChild(canvas);
+
 }, 250);
+
+function loadImage(url) {
+	return new Promise(resolve => {
+		var img = new Image();
+		// Size of the source SVG
+		img.width = 731 * 3;
+		img.height = 297 * 3;
+		img.onload = function() { resolve(img); }
+		img.src = url;
+	});
+}
 
 function downloadUri(filename, uri) {
 	var link = document.createElement("a");
@@ -62,11 +97,26 @@ function downloadUri(filename, uri) {
 }
 
 async function generateDownload() {
-	let canvas = await html2canvas(fullLogo);
-	let dataUri = canvas.toDataURL('image/png');
-  downloadUri("Yes.png", dataUri)
+  downloadUri("yes.png", canvas.toDataURL('image/png'));
 }
 
-selectElementContents(subHead);
-subHead.addEventListener("input", update);
+var params = new URLSearchParams(window.location.search);
+
+if (params.has("name")) {
+	$("#name").value = params.get("name");
+}
+
+if (params.has("layout")) {
+	let layout = params.get("layout");
+	let radio = $(`input[name=layout][value=${layout}]`).checked = true;
+}
+
+$("#name").addEventListener("input", update);
+$("#layout").addEventListener("input", update);
 download.addEventListener("click", generateDownload);
+
+(async function () {
+	await document.fonts.ready;
+	await document.fonts.load("10pt gilroyblack");
+	update();
+})();
